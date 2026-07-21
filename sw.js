c095d2dcc0421088be51630a4f342101d8f01a4e
so0yeonnn <306534650+so0yeonnn@users.noreply.github.com>
2026-07-21 22:44:43 +0900

    18회차 40문항 개별 검수 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'istqb-ctfl-reviewed-14-sets';
+const CACHE = 'istqb-ctfl-reviewed-15-sets';
 const ASSETS = ['./', './index.html', './app.css', './bank.js', './app-v2.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
